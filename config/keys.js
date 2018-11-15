feb621b7d8b39679168e338d4e208efc654057b7
shuraim26 <mdshuraim2013@gmail.com>
2018-11-15 21:42:52 +0530

    OAuth

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports={
+    mongoURI:"mongodb://root:password123@ds039281.mlab.com:39281/storybooks_db",
+    googleClientID:"465531482671-3l7q52r0ltpdfgcgsa7n1vsf6bs2vti9.apps.googleusercontent.com",
+    googleClientSecret:"tAm7B7sdT5CTCWjswrljfCl9"
+}
