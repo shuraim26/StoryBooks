81bfa90e54f2f9dec83658715c134fddc7104afd
shuraim26 <mdshuraim2013@gmail.com>
2018-11-21 14:12:25 +0530

    started UI

--- app.js
+++ app.js
@@ -2,21 +2,66 @@
 const express=require("express");
 const mongoose=require("mongoose");
 const passport=require("passport");
+const cookieParser=require("cookie-parser");
+const session=require("express-session");
+const exphbs=require("express-handlebars");
+
+//Load keys
+const keys=require("./config/keys");
+
+//Load user model
+require("./models/User");
 
 //Passport config
 require("./config/passport")(passport);
 
+//Mongoose connect
+mongoose.connect(keys.mongoURI,{
+    useNewUrlParser: true
+})
+.then(function(){
+    console.log("MongoDB Connected!");
+})
+.catch(function(err){
+    console.log("Error in Connection with MongoDB!");
+});
+
 //Load routes
 const auth=require("./routes/auth");
+const index=require("./routes/index");
 
 const app=express();
 
-app.get("/",(req,res) => {
-    res.send("it works!");
+//Handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//Cookie-parser middleware
+app.use(cookieParser());
+
+//Express-session middleware
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Map global promises
+mongoose.Promise = global.Promise;
+
+//Set global vars
+app.use((req,res,next) => {
+    res.locals.user=req.user || null;
+    next();
 });
 
 //Use routes
 app.use("/auth",auth);
+app.use("/",index);
 
 const port=process.env.PORT || 5000;
 
--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,19 @@ router.get('/google/callback',
   passport.authenticate('google', { failureRedirect: '/' }),
   function(req, res) {
     // Successful authentication, redirect home.
-    res.redirect('/dashbord');
-  });
+    res.redirect('/dashboard');
+});
+
+router.get("/verify",(req,res) => {
+  if(req.user)
+    console.log(req.user);
+  else
+    console.log("Not auth");
+});
+
+router.get("/logout",(req,res) => {
+  req.logout();
+  res.redirect("/");
+});
 
 module.exports=router;
