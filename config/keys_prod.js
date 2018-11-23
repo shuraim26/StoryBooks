fec5286816ca3ad4290be89810b0faa37a64c6cf
shuraim26 <mdshuraim2013@gmail.com>
2018-11-23 20:06:32 +0530

    Finalized

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,5 @@
 module.exports={
     mongoURI:process.env.MONGO_URI,
-    googleClientID:process.GOOGLE_CLIENT_ID,
+    googleClientID:process.env.GOOGLE_CLIENT_ID,
     googleClientSecret:process.env.GOOGLE_CLIENT_SECRET
 }
